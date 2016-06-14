57480a224a04dbaf33c2935333a190317d557108
Tom Howard <tom@windyroad.com.au>
2016-06-15 07:18:45 +1000

    switched from debugging logging to info logging

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    gradle uploadArchives -Psigning.secretKeyRingFile=cd/codesigning.gpg --debug
+    gradle uploadArchives -Psigning.secretKeyRingFile=cd/codesigning.gpg --info
 fi

9c1bb3dade18b1fb9743582c161e6aa32be19b64
Tom Howard <tom@windyroad.com.au>
2016-06-16 23:38:27 +1000

    still trying to get the travis build working

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+set -e
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    ./gradlew --daemon uploadArchives -Psigning.secretKeyRingFile=cd/codesigning.gpg --info
+    ./gradlew --daemon uploadArchives "-Psigning.keyId=$GPG_KEY_ID" "-Psigning.password=$GPG_PASS" "-Psigning.secretKeyRingFile=cd/codesigning.gpg" --info
 fi

d6498b50003c97234cda590f7d64dab4f1843dc3
Tom Howard <tom@windyroad.com.au>
2016-06-17 16:43:19 +1000

    still trying to get code signing working

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -2,9 +2,12 @@
 
 set -e
 
+
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+   cd cd
    gpg --list-keys
-   openssl aes-256-cbc -K $encrypted_86a80f7728b7_key -iv $encrypted_86a80f7728b7_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
-   gpg --fast-import cd/codesigning.asc
+   openssl aes-256-cbc -K $encrypted_86a80f7728b7_key -iv $encrypted_86a80f7728b7_iv -in codesigning.asc.enc -out codesigning.asc -d
+   gpg --fast-import codesigning.asc
    gpg --list-keys
 fi

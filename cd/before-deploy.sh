1652ab19cdb195da3c3513e1cf645eb8d14a10e8
Tom Howard <tom@windyroad.com.au>
2016-06-17 10:09:57 +1000

    still trying to get travis build working

--- cd/before-deploy.sh
+++ cd/before-deploy.sh
@@ -3,6 +3,7 @@
 set -e
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-   openssl aes-256-cbc -K $encrypted_994e8d47c447_key -iv $encrypted_994e8d47c447_iv -in codesigning.asc.enc -out cd/codesigning.asc -d
+   openssl aes-256-cbc -K $encrypted_994e8d47c447_key -iv $encrypted_994e8d47c447_iv -in cd/codesigning.gpg.enc -out cd/codesigning.gpg -d
+   openssl aes-256-cbc -K $encrypted_994e8d47c447_key -iv $encrypted_994e8d47c447_iv -in cd/codesigning.asc.enc -out cd/codesigning.asc -d
    gpg --fast-import cd/codesigning.asc
 fi

075b18103aba52403d0bf2579b7d8f5f520032ae
Tom Howard <tom@windyroad.com.au>
2016-06-27 10:18:56 +1000

    fixing OSS RH creds

--- cd/deploy.sh
+++ cd/deploy.sh
@@ -9,7 +9,5 @@ if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; the
     echo "signing.keyId=$GPG_KEY_ID" >> $PROP_FILE
     echo "signing.password=$GPG_PASS" >> $PROP_FILE
     echo "signing.secretKeyRingFile=$HOME/.gnupg/secring.gpg" >> $PROP_FILE
-    echo "ossrhUsername=$OSSRH_USERNAME" >> $PROP_FILE
-    echo "ossrhPassword=$OSSRH_PASSWORD" >> $PROP_FILE
-    ./gradlew uploadArchives --stacktrace
+    ./gradlew uploadArchives
 fi
